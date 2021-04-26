887c5d2795f278c9ef8ed86158ffb556ab9ea3c2
Damian Llodra <damianllodra@hotmail.com>
2021-04-26 12:01:00 -0300

    first

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'aa181ec9d6e1383565dfe24982c2adf6'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7491c73e-8eda-4840-b317-b4f41a286bc0'
+};
