53cb41c99a4d10c654af916409927b17019633ec
Damian Llodra <damianllodra@hotmail.com>
2021-06-28 09:26:33 -0300

    catalogo

--- api/articulos/controllers/articulos.js
+++ api/articulos/controllers/articulos.js
@@ -1,8 +1,24 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async catalogue(ctx) {
+    const products = await strapi.query("articulos").find();
+
+    const catalogue = products.map((product) => {
+      return {
+        id: product.id,
+        nombre: product.descripcion,
+        imagen: product.imagenes[0],
+        precio: product.precio_final,
+        categoria: product.categoria ? product.categoria.nombre : "Otros",
+      };
+    });
+
+    ctx.send({ message: "Ok", data: catalogue }, 200);
+  },
+};
